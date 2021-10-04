2a78435c80bf089e614c5311637273689f1fcbf1
DileepGobburi <dileepreddygobburi@gmail.com>
2021-10-04 16:32:20 +0530

    adding node at begin

---
+++ sll/add_bgn.c
@@ -0,0 +1,11 @@
+#include"header.h"
+void add_bgn()
+{
+	new=(struct node*)malloc(sizeof(struct node));
+	printf("enter data\n");
+	scanf("%d",&value);
+	new->data=value;
+	new->next=head;
+	head=new;
+}
+
