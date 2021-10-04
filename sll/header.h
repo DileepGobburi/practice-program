52876fb75b1c055158245898604b6dffa556f3e2
DileepGobburi <dileepreddygobburi@gmail.com>
2021-10-04 16:36:46 +0530

    header file for sll

---
+++ sll/header.h
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+	{
+		int data;
+		struct node *next;
+	}*new,*head,*tail,*temp;
+int value;
+void add_bgn();
+void add_end(); 
+void dte_bgn();
+void dte_end();
+void display();
+
