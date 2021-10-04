cdd5912d22b34ffe6444fcf63435927fb65e1558
DileepGobburi <dileepreddygobburi@gmail.com>
2021-10-04 08:24:56 +0530

    main function

---
+++ mock2/main.c
@@ -0,0 +1,36 @@
+#include"head.c"
+int main()
+{
+int i,j,k,*ptr;
+srand(getpid());
+	for(i=0;i<4;i++)
+ 	{
+		for(j=0;j<3;j++)
+		{
+		
+ 	   		for(k=0;k<2;k++)
+			{
+				s.ar[i][j][k]= rand();
+			}
+			
+		}
+	}
+
+	for(i=0;i<4;i++)
+ 	{
+		for(j=0;j<3;j++)
+		{
+		
+ 	   		for(k=0;k<2;k++)
+			{
+				printf("%d\n",s.ar[i][j][k]);
+				//int temp =p[i][j][k];
+			}
+			
+		}
+	}
+	
+	 var(s);
+	
+}
+
