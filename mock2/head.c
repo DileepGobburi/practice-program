55549edf834d42d4f9f1cd4aef98115cb9afbebc
DileepGobburi <dileepreddygobburi@gmail.com>
2021-10-04 08:24:13 +0530

    headerfile

---
+++ mock2/head.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct student
+	{
+	 	int ar[4][3][2];
+	 	
+	}s;
+extern char str1[20],str2[20];
+extern int count;
+void var(struct student s);
+void pattern(int x);
+int my_strcmp(char *m,char *n);
+void tree();
+
