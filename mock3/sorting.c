dea6251532cc3d6e5efbd2446d0f354d23eb0763
DileepGobburi <90783859+DileepGobburi@users.noreply.github.com>
2021-11-17 17:42:40 +0530

    "insertion sort"

--- mock3/sorting.c
+++ mock3/sorting.c
@@ -1,8 +1,37 @@
-#include <stdio.h>
-#define R 3
-#define C 3
-void Input(int mat[][C]);
-void Print(int mat[][C]);
-void Multiply(int mat1[][C], int mat2[][C], int res[][C]);
-void sorting(int b[][C]);
 
+
+#include"header.h"
+void sorting(int b[][C])
+{	
+int a[10];
+ int i,j,k=0,temp,sum=0;
+ 
+ for(i=0;i<R;i++)
+ 	{
+ 		for(j=0;j<C;j++)
+ 		{
+ 		a[k]=b[i][j];
+ 		k++;
+ 		}
+
+ 	}
+ 	printf(" before insertion sorting\n");
+ for(i=0;i<9;i++)
+printf(" %d",a[i]);
+printf("\n");
+for(i=1;i<9;i++)
+	{
+		temp=a[i];
+		j=i-1;
+		while(j>=0 && a[j]<temp)
+			{
+				a[j+1]=a[j];
+				j--;
+			}
+			a[j+1]=temp;
+	}
+printf("elements after insertion sorting\n");
+for(i=0;i<9;i++)
+printf(" %d",a[i]);
+printf("\n");
+}
